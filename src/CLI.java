74fa8e8b85378f889d6d0fdc975b58d249bf1871
Raygunito <112569656+Raygunito@users.noreply.github.com>
2024-11-29 11:03:07 +0100

    feat : add KNN for k 1 to 12 with stats

--- src/CLI.java
+++ src/CLI.java
@@ -1,21 +1,76 @@
 import java.util.ArrayList;
+import java.util.Set;
 
 import data.CharacteristicVector;
 import data.Classifier;
+import data.ConfusionMatrix;
 import data.EntityConstants;
 import process.KNNClassifier;
 import utils.DataLoader;
+import utils.MachineLearningUtils;
 
 public class CLI {
-    private static final String ART_FILE_LOCATION = "SharvitB2\\Signatures\\ART";
-    private static final String E34_FILE_LOCATION = "SharvitB2\\Signatures\\E34";
+    private static final String ART_FILE_LOCATION = "res\\Signatures\\ART";
+    @SuppressWarnings("unused")
+    private static final String E34_FILE_LOCATION = "res\\Signatures\\E34";
+    private static final String GFD_FILE_LOCATION = "res\\Signatures\\GFD";
+    private static final String YNG_FILE_LOCATION = "res\\Signatures\\Yang";
+    private static final String ZRK_FILE_LOCATION = "res\\Signatures\\Zernike7";
 
     public static void main(String[] args) {
-        KNNClassifier knnClassifier = new KNNClassifier(5, Classifier.EUCLIDEAN);
-        ArrayList<CharacteristicVector> array = DataLoader.extractFromFolder(E34_FILE_LOCATION);
-        CharacteristicVector cVector2 = array.remove(21);
-        knnClassifier.train(array);
-        String prediction = knnClassifier.predict(cVector2);
-        System.out.println("Found : " + EntityConstants.getEntityByLabelCode(prediction));
+        int nbClass = 10;
+        int nbEchantillon = 12;
+        String[] location = { ART_FILE_LOCATION, GFD_FILE_LOCATION, YNG_FILE_LOCATION, ZRK_FILE_LOCATION };
+        for (String stringPath : location) {
+            System.out.println("Doing KNN to folder " + stringPath);
+            doKNN(stringPath, nbClass * nbEchantillon, Classifier.EUCLIDEAN, true);
+        }
+
+    }
+
+    public static void doKNN(String folderLocation, int datasetSize, String distanceMetric,
+            boolean cutDatasetTo10Classes) {
+        Set<String> entites = EntityConstants.entities.keySet();
+        if (cutDatasetTo10Classes) {
+            // Data set containt 18 classes, 12 sample each so we work on only the first 10
+            // classes for the project
+            entites.remove("11");
+            entites.remove("12");
+            entites.remove("13");
+            entites.remove("14");
+            entites.remove("15");
+            entites.remove("16");
+            entites.remove("17");
+            entites.remove("18");
+        }
+        ConfusionMatrix cfx = new ConfusionMatrix(entites);
+        ArrayList<CharacteristicVector> dataset = new ArrayList<>(
+                DataLoader.extractFromFolder(folderLocation).subList(0, datasetSize));
+
+        // Should we shuffle since we LOOCV inherit the fact that we check for each point ?
+        // Collections.shuffle(dataset);
+
+        int bestK = 1;
+        double bestF1Score = Double.MIN_VALUE;
+        for (int k = 1; k < 12; k++) {
+            ConfusionMatrix testCfMatrix = new ConfusionMatrix(entites);
+            KNNClassifier knn = new KNNClassifier(k, distanceMetric);
+            knn.train(dataset);
+            MachineLearningUtils.performLOOCV(dataset, knn, testCfMatrix);
+            double res = testCfMatrix.globalF1Score() * 100;
+            // double res = MachineLearningUtils.performLOOCV(dataset, knn) * 100;
+            if (res > bestF1Score) {
+                bestF1Score = res;
+                bestK = k;
+            }
+            System.out.println("k=" + k + ",F1-Score : " + String.format("%.2f", res) + "%");
+        }
+
+        KNNClassifier classifier = new KNNClassifier(bestK, distanceMetric);
+        classifier.train(dataset);
+        MachineLearningUtils.performLOOCV(dataset, classifier, cfx);
+
+        cfx.display();
+        cfx.showPerformance();
     }
 }
--- src/utils/MachineLearningUtils.java
+++ src/utils/MachineLearningUtils.java
@@ -2,17 +2,24 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+
 import data.CharacteristicVector;
 import data.Classifier;
 import data.ConfusionMatrix;
+import data.EntityConstants;
+import logger.LoggerUtil;
 
 public class MachineLearningUtils {
+    private static final Logger logger = LoggerUtil.getLogger(MachineLearningUtils.class, Level.INFO);
 
     /**
      * Performs k-fold cross-validation on the given dataset.
      *
-     * @param dataset   The dataset to be split into k folds.
-     * @param k         The number of folds.
+     * @param dataset The dataset to be split into k folds.
+     * @param k       The number of folds.
      * @return A list of k folds, each containing a list of CharacteristicVectors.
      */
     public static ArrayList<ArrayList<CharacteristicVector>> createKFolds(
@@ -33,9 +40,11 @@ public class MachineLearningUtils {
     }
 
     /**
-     * Performs Leave-One-Out Cross-Validation (LOOCV) on the given dataset with a specified classifier.
+     * Performs Leave-One-Out Cross-Validation (LOOCV) on the given dataset with a
+     * specified classifier.
      * Do not split your dataset (i.e. no 80/10/10 or anything else)
-     * @param dataset   The dataset to be split into LOOCV folds.
+     * 
+     * @param dataset    The dataset to be split into LOOCV folds.
      * @param classifier The classifier to be used for training and validation.
      * @return The accuracy of the classifier over all folds.
      */
@@ -45,7 +54,7 @@ public class MachineLearningUtils {
             // Split dataset into training and validation set
             ArrayList<CharacteristicVector> trainingSet = new ArrayList<>(dataset);
             CharacteristicVector validationSet = trainingSet.remove(i);
-            
+
             classifier.train(trainingSet);
 
             String predictedLabel = classifier.predict(validationSet);
@@ -54,21 +63,35 @@ public class MachineLearningUtils {
                 correctPredictions++;
             }
         }
-        
+
         return (double) correctPredictions / dataset.size();
     }
 
-    public static void performLOOCV(ArrayList<CharacteristicVector> dataset, Classifier classifier,ConfusionMatrix cfx) {
+    public static void performLOOCV(ArrayList<CharacteristicVector> dataset, Classifier classifier,
+            ConfusionMatrix cfx) {
+        if (dataset == null || dataset.isEmpty()) {
+            logger.error("Dataset is null or empty. LOO-CV cannot proceed.");
+            return;
+        }
+        logger.debug("Starting Leave-One-Out Cross-Validation (LOO-CV) with classifier: {}",
+                classifier.getClass().getSimpleName());
+        logger.debug("Dataset size: {}", dataset.size());
         for (int i = 0; i < dataset.size(); i++) {
             // Split dataset into training and validation set
             ArrayList<CharacteristicVector> trainingSet = new ArrayList<>(dataset);
             CharacteristicVector validationSet = trainingSet.remove(i);
-            
+            String validationLabel = validationSet.getLabel();
             classifier.train(trainingSet);
 
             String predictedLabel = classifier.predict(validationSet);
+            logger.debug("Processed {}th point. Actual: {} ({}) | Predicted: {} ({})",
+                    i + 1,
+                    validationLabel,
+                    EntityConstants.getEntityByLabelCode(validationLabel),
+                    predictedLabel,
+                    EntityConstants.getEntityByLabelCode(predictedLabel));
             // Compare predicted label with actual label
-            cfx.increment(validationSet.getLabel(), predictedLabel);
-        }        
+            cfx.increment(validationLabel, predictedLabel);
+        }
     }
 }
